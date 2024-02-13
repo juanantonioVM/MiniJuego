6bc527e33ec5f45277e2b632d1dea3f4541e6411
Juan Antonio Dólera Jiménez <juanantonio.dolera.jimenez@ces-vegamedia.es>
2024-02-13 12:19:52 +0100

    Recursos

---
+++ VegaJuego/Level1.cs
@@ -0,0 +1,15 @@
+namespace VegaJuego
+{
+    public partial class Level1 : Form
+    {
+        public Level1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
