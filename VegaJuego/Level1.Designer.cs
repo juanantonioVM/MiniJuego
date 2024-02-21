b799827ae8486b0cbb8283022ab237ca140c390b
Juan Antonio Dólera Jiménez <juanantonio.dolera.jimenez@ces-vegamedia.es>
2024-02-21 13:51:28 +0100

    PictureBox y labels

--- VegaJuego/Level1.Designer.cs
+++ VegaJuego/Level1.Designer.cs
@@ -31,6 +31,15 @@
             components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Level1));
             MovEnemigos = new System.Windows.Forms.Timer(components);
+            pictureBox1 = new PictureBox();
+            labelVida = new Label();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            labelEscudo = new Label();
+            labelArma = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             SuspendLayout();
             // 
             // MovEnemigos
@@ -38,12 +47,87 @@
             MovEnemigos.Enabled = true;
             MovEnemigos.Tick += timer1_Tick_1;
             // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImage = Properties.Resources.salud;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Center;
+            pictureBox1.Location = new Point(683, 41);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(43, 39);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // labelVida
+            // 
+            labelVida.AutoSize = true;
+            labelVida.BackColor = Color.Transparent;
+            labelVida.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            labelVida.ForeColor = Color.Gold;
+            labelVida.Location = new Point(732, 54);
+            labelVida.Name = "labelVida";
+            labelVida.Size = new Size(36, 17);
+            labelVida.TabIndex = 1;
+            labelVida.Text = "Vida";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.BackgroundImage = Properties.Resources.escudo;
+            pictureBox2.BackgroundImageLayout = ImageLayout.Center;
+            pictureBox2.Location = new Point(683, 77);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(43, 39);
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.Transparent;
+            pictureBox3.BackgroundImage = Properties.Resources.Arma;
+            pictureBox3.BackgroundImageLayout = ImageLayout.Center;
+            pictureBox3.Location = new Point(683, 122);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(43, 39);
+            pictureBox3.TabIndex = 3;
+            pictureBox3.TabStop = false;
+            // 
+            // labelEscudo
+            // 
+            labelEscudo.AutoSize = true;
+            labelEscudo.BackColor = Color.Transparent;
+            labelEscudo.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            labelEscudo.ForeColor = Color.Gold;
+            labelEscudo.Location = new Point(732, 90);
+            labelEscudo.Name = "labelEscudo";
+            labelEscudo.Size = new Size(51, 17);
+            labelEscudo.TabIndex = 4;
+            labelEscudo.Text = "Escudo";
+            // 
+            // labelArma
+            // 
+            labelArma.AutoSize = true;
+            labelArma.BackColor = Color.Transparent;
+            labelArma.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            labelArma.ForeColor = Color.Gold;
+            labelArma.Location = new Point(732, 132);
+            labelArma.Name = "labelArma";
+            labelArma.Size = new Size(41, 17);
+            labelArma.TabIndex = 5;
+            labelArma.Text = "Arma";
+            // 
             // Level1
             // 
             AutoScaleMode = AutoScaleMode.None;
             BackgroundImage = Properties.Resources.FondoFinal;
             BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(806, 467);
+            Controls.Add(labelArma);
+            Controls.Add(labelEscudo);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(labelVida);
+            Controls.Add(pictureBox1);
             DoubleBuffered = true;
             Icon = (Icon)resources.GetObject("$this.Icon");
             MaximizeBox = false;
@@ -52,11 +136,21 @@
             Text = "Form1";
             Load += Form1_Load;
             KeyPress += Level1_KeyPress;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private System.Windows.Forms.Timer MovEnemigos;
+        private PictureBox pictureBox1;
+        private Label labelVida;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private Label labelEscudo;
+        private Label labelArma;
     }
 }
